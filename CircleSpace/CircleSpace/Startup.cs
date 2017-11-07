939a40dfc1521192d58c711580ad45ba48c24f40
Chrstian Moore <CMoore@student.neumont.edu>
2017-11-07 16:18:11 -0700

    Initialized project

---
+++ CircleSpace/CircleSpace/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CircleSpace.Startup))]
+namespace CircleSpace
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
