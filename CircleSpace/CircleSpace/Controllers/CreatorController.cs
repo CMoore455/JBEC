2d7920b647d6f21467ec857f634c46d6b35b8689
Errlin Deleon <EDeleon@student.neumont.edu>
2017-11-21 13:10:26 -0700

    Closes #18

--- CircleSpace/CircleSpace/Controllers/CreatorController.cs
+++ CircleSpace/CircleSpace/Controllers/CreatorController.cs
@@ -1,4 +1,5 @@
-﻿using CircleSpaceServiceLib.Service;
+﻿using CircleSpaceGeneralModels.Models;
+using CircleSpaceServiceLib.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Web.Mvc;
 namespace CircleSpace.Controllers
 {
 
-    [Authorize]
+    //[Authorize]
     public class CreatorController : Controller
     {
         private ICircleSpaceService service;
@@ -21,7 +22,9 @@ namespace CircleSpace.Controllers
         public ActionResult CreateWebPage()
         {
             //Needs a List<Layouts> that will become option to be selected and applied to the live preview Window
-            return View(service.GetLayouts());
+            return View(new List<LayoutModel>() { new LayoutModel() { ID = 2, LayoutTitle = "Header", Type = CircleSpaceGeneralModels.Enums.LayoutTypes.Header },
+                new LayoutModel() { ID = 3, LayoutTitle = "Body", Type = CircleSpaceGeneralModels.Enums.LayoutTypes.Body },
+                new LayoutModel() { ID = 1, LayoutTitle = "Footer", Type = CircleSpaceGeneralModels.Enums.LayoutTypes.Footer } });//service.GetLayouts());
         }
     }
 }
