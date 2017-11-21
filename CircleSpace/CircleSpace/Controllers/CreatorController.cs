66f4782ea6e84fc051e22f852e580f16662ca857
Nilrre <EDeleon@student.neumont.edu>
2017-11-21 13:20:11 -0700

    Update CreatorController.cs

--- CircleSpace/CircleSpace/Controllers/CreatorController.cs
+++ CircleSpace/CircleSpace/Controllers/CreatorController.cs
@@ -22,10 +22,10 @@ namespace CircleSpace.Controllers
         public ActionResult CreateWebPage()
         {
             //Needs a List<Layouts> that will become option to be selected and applied to the live preview Window
-            return View(
-            /*new List<LayoutModel>() { new LayoutModel() { ID = 2, LayoutTitle = "Header", Type = CircleSpaceGeneralModels.Enums.LayoutTypes.Header },
-                new LayoutModel() { ID = 3, LayoutTitle = "Body", Type = CircleSpaceGeneralModels.Enums.LayoutTypes.Body },
-                new LayoutModel() { ID = 1, LayoutTitle = "Footer", Type = CircleSpaceGeneralModels.Enums.LayoutTypes.Footer }*/ });//service.GetLayouts());
+            return View(service.GetLayouts());
+            //new List<LayoutModel>() { new LayoutModel() { ID = 2, LayoutTitle = "Header", Type = CircleSpaceGeneralModels.Enums.LayoutTypes.Header },
+              //  new LayoutModel() { ID = 3, LayoutTitle = "Body", Type = CircleSpaceGeneralModels.Enums.LayoutTypes.Body },
+              //  new LayoutModel() { ID = 1, LayoutTitle = "Footer", Type = CircleSpaceGeneralModels.Enums.LayoutTypes.Footer } });
         }
     }
 }
