f389634e470ec7d39507db1fcb1d974fa93f8be7
Jeffrey Henderson <jayhenderson758@gmail.com>
2017-12-07 00:50:15 -0700

    Added save ability to the editlayout page.

---
+++ CircleSpace/CircleSpace/Models/JSONForSavingLayout.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using CircleSpaceGeneralModels.Enums;
+using System.Web.Mvc;
+
+namespace CircleSpace.Models
+{
+    public class JSONForSavingLayout
+    {
+        string title, content, css;
+        int id;
+        LayoutTypes type;
+
+        public string Title { get; set; }
+        [AllowHtml]
+        public string Content { get;  set; }
+        public int ID { get;  set; }
+        public string CSS { get; set; }
+        public List<string> Tags { get; set; }
+        public LayoutTypes Type { get;  set; }
+    }
+}
--- CircleSpace/CricleSpaceGeneralModels/Models/LayoutModel.cs
+++ CircleSpace/CricleSpaceGeneralModels/Models/LayoutModel.cs
@@ -66,7 +66,6 @@ namespace CircleSpaceGeneralModels.Models
             set { tags = value; }
         }
 
- 
     }
 
 }
